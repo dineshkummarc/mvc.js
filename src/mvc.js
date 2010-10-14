e0d08228d4f8456f7aafad0df7bdd2d7cc04f367
Richard Layte <rich.layte@gmail.com>
2010-10-14 17:49:40 +0100

    startup complete events being triggered

--- src/mvc.js
+++ src/mvc.js
@@ -1,5 +1,9 @@
 var mvc = {
     create: function(context) {
+        this.events.register('startup_complete');   
+        
         context();
+        
+        this.events.dispatch('startup_complete')
     }
 }
--- tests/mvc_tests.js
+++ tests/mvc_tests.js
@@ -2,10 +2,31 @@ TestCase("init", {
     setUp: function(){
         context = xray_specs.stub();
         
+        xray_specs.mock(mvc, 'events', {
+            register: {},
+            dispatch: {}
+        });
+        
         mvc.create(context);
+    },
+    
+    "test that a startup_complete signal is registered": function(){
+        mvc.events.expects('register')
+          .to_be_called.times(1)
+            .with_args.including('startup_complete');
+            
+        assertTrue(mvc.events.verify());
     },
     
     "test that context function is called": function(){
         assertEquals(1, context.called_exactly(1));
+    },
+    
+    "test that once context has been set up a startup_complete event is triggered": function(){
+        mvc.events.expects('dispatch')
+          .to_be_called.times(1)
+            .with_args.including('startup_complete');
+            
+        assertTrue(mvc.events.verify());
     }
 });
