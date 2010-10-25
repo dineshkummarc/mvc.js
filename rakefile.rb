5867c469931de76f2ce5ffe4bffbb43afb73a60c
Richard Layte <rich.layte@gmail.com>
2010-10-25 13:51:05 +0100

    removed jquery from libs

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,9 @@
 
+task :tests do
+  system 'java -jar $JSTD_HOME/JsTestDriver-1.2.2.jar --port 3232'
+  system 'java -jar $JSTD_HOME/JsTestDriver-1.2.2.jar --tests all --reset'
+end
+
 task :doc do
   system 'java -jar $JSDOC_TOOLKIT/jsrun.jar $JSDOC_TOOLKIT/app/run.js -a -t=$JSDOC_TOOLKIT/templates/mvc src/mvc.js -d=docs'
 end
