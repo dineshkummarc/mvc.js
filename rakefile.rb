efec7b8a096d798f9eb584b666b42233d3f9458d
Richard Layte <rich.layte@gmail.com>
2010-10-26 16:00:44 +0100

    testing that gh-pages branch is also updated

--- rakefile.rb
+++ rakefile.rb
@@ -1,29 +1,34 @@
+commit_message = 'not defined'
 
 task :create_docs do
   system 'java -jar $JSDOC_TOOLKIT/jsrun.jar $JSDOC_TOOLKIT/app/run.js -a -t=docs/template -d=docs/output src/mvc.js'
+  
+  Rake::Task[:minify].invoke()
 end
 
 task :minify do
   system 'java -jar $YUI_COMPRESSOR/build/yuicompressor-2.4.2.jar -o src/mvc.min.js src/mvc.js'
+  
+  Rake::Task[:commit].invoke()
 end
 
 task :commit, :message do |t, args|
-  system 'git commit -am "' + args[:message] + '"'
+  system 'git commit -am "' + commit_message + '"'
   system 'git push origin master'
   
   Rake::Task[:create_gh_page].invoke(args[:message])
 end
 
 task :create_gh_page, :message do |t, args|
-  puts args[:message]
-  
   system 'git checkout gh-pages'
   system 'git checkout master docs/output'
-  system 'git commit -am "' + args[:message] + '"'
+  system 'git commit -am "' + commit_message + '"'
   system 'git push origin gh-pages'
   system 'git checkout master'
 end
 
 task :build, :message do |t, args|
-  Rake::Task[:commit].invoke(args[:message])
+  commit_message = args[:message]
+  
+  Rake::Task[:create_docs].invoke()
 end
