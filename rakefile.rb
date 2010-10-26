6f6b5d78beed2d52042db872a74ada08be61557f
Richard Layte <rich.layte@gmail.com>
2010-10-26 12:01:38 +0100

    added first steps to automatic gh-page creation in rake file

--- rakefile.rb
+++ rakefile.rb
@@ -7,4 +7,9 @@ task :minify do
   system 'java -jar $YUI_COMPRESSOR/build/yuicompressor-2.4.2.jar -o src/mvc.min.js src/mvc.js'
 end
 
-task :default => [:create_docs, :minify]
+task :create_gh_page do 
+  system 'git checkout gh-pages'
+  system 'git checkout master docs/output examples'
+end
+
+task :default => [:create_docs, :minify, :create_gh_page]
