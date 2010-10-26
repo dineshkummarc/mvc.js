833d41af9e489ffc6fdb54673804999c6d305dab
Richard Layte <rich.layte@gmail.com>
2010-10-26 12:58:21 +0100

    testing rake build commit

--- rakefile.rb
+++ rakefile.rb
@@ -7,9 +7,17 @@ task :minify do
   system 'java -jar $YUI_COMPRESSOR/build/yuicompressor-2.4.2.jar -o src/mvc.min.js src/mvc.js'
 end
 
+task :commit do
+  system 'git commit -am "testing rake build commit"'
+  system 'git push origin master'
+end
+
 task :create_gh_page do 
   system 'git checkout gh-pages'
   system 'git checkout master docs/output examples'
+  system 'git commit -am "testing rake build commit"'
+  system 'git push origin gh-pages'
+  system 'git checkout master'
 end
 
-task :default => [:create_docs, :minify, :create_gh_page]
+task :default => [:create_docs, :minify, :commit, :create_gh_page]
