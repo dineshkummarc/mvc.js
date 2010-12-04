da0a8928f3417e61f1aa5bc362eddeddc083d77e
Richard Layte <rich.layte@gmail.com>
2010-12-04 13:16:22 +0000

    model dispatch feature added

--- tests/features/step_definitions/page_steps.rb
+++ tests/features/step_definitions/page_steps.rb
@@ -6,3 +6,12 @@ Then 'I should see 0 on the display' do
     count = @browser.div(:class, 'count')
     count.text.should == '0'
 end
+
+When 'I click the add button' do 
+    @browser.button(:name, 'add').click
+end
+
+Then 'I should see 1 on the display' do
+    count = @browser.div(:class, 'count')
+    count.text.should == '1'
+end
