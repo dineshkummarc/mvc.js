a7d8e380b7f6d920aaa74ca154fb9e6b4ba654b2
Richard Layte <rich.layte@gmail.com>
2011-01-22 23:53:57 +0000

    fixed merge bug in shopping cart example

--- examples/shopping_cart/src/app.js
+++ examples/shopping_cart/src/app.js
@@ -1,10 +1,6 @@
 var shopping_cart = {
 
-<<<<<<< HEAD
     config: {
-=======
-    values: {
->>>>>>> 99638be2dc0a61ab22a4292768214b1070697d52
         cart_view: $('.cart'),
         items_view: $('.items')
     }
