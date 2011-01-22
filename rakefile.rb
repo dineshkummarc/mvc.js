a7f9c4401320ec27b4deb3231428daee4b63d111
Richard Layte <rich.layte@gmail.com>
2011-01-22 18:49:54 +0000

    changed src folder to lib

--- rakefile.rb
+++ rakefile.rb
@@ -2,26 +2,17 @@ require 'rdiscount'
 
 task :create_docs do
   puts 'creating inline docs'
-  sh 'java -jar $JSDOC_TOOLKIT/jsrun.jar $JSDOC_TOOLKIT/app/run.js -a -t=docs/template -d=docs/output src/mvc.js'
+  sh 'java -jar $JSDOC_TOOLKIT/jsrun.jar $JSDOC_TOOLKIT/app/run.js -a -t=docs/template -d=docs/output lib/mvc.js'
 end
 
 task :minify do
   puts 'minifying javascript'
-  sh 'java -jar $YUI_COMPRESSOR/build/yuicompressor-2.4.2.jar -o src/mvc.min.js src/mvc.js'
+  sh 'java -jar $YUI_COMPRESSOR/build/yuicompressor-2.4.2.jar -o lib/mvc.min.js lib/mvc.js'
 end
 
 task :unit_tests do
   puts 'running unit tests'
   sh 'java -jar ~/Library/JsTestDriver/JsTestDriver-1.2.2.jar --port 3232 --browser $FIREFOX --tests all --reset' do |ok, res|
-    if not ok
-      return false
-    end
-  end
-end
-
-task :func_tests do
-  puts 'running functional tests'
-  sh 'cucumber tests/features/' do |ok, res|
     if not ok
       return false
     end
@@ -49,7 +40,7 @@ task :create_ghpages, :message do |t, args|
   puts 'transfering code to gh-pages'
 
   sh 'git checkout gh-pages'
-  sh 'git checkout master src/'
+  sh 'git checkout master lib/'
   sh 'git checkout master docs/'
   sh 'git checkout master examples/'
   sh 'git commit -am "' + args.message + '"'
