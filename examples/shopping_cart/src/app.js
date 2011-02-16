14bd932515d83a8e0f851f73cb75a00f1216412d
Richard Layte <rich.layte@gmail.com>
2011-02-16 20:39:12 +0000

    restructured examples folder

--- examples/shopping_cart/src/app.js
+++ examples/shopping_cart/src/app.js
@@ -1,8 +1,3 @@
 var shopping_cart = {
-
-    config: {
-        cart_view: $('.cart'),
-        items_view: $('.items')
-    }
  
 };
--- examples/shopping_cart/src/controllers.js
+++ examples/shopping_cart/src/controllers.js
@@ -1,20 +1,3 @@
 shopping_cart.controllers = {
 
-    add_item: {
-        cart: '__inject__',
-
-        command: function(item) {
-            var album_info = $(item).find('img').attr('alt').split(' - ');
-            
-            var album = {
-                title: album_info[1],
-                artist: album_info[0],
-                price: 9.99,
-                quantity: 1
-            }
-            
-            this.cart.add_item(album);
-        }
-    }
-
 }
