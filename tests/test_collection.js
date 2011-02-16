1971d4bd98fdce1c5a59983c47f2d11e13bcea1c
Richard Layte <rich.layte@gmail.com>
2011-02-16 21:27:53 +0000

    added collections object

--- tests/test_collection.js
+++ tests/test_collection.js
@@ -1,11 +1,15 @@
 TestCase('collections', {
 
     setUp: function() {
-        
+        collection = mvc.collections();
     },
 
     'test that mvc.collection is a function': function() {
         assertFunction(mvc.collections);
+    },
+
+    'test that it return an object with a create method': function() {
+        assertFunction(collections.create);
     }
 
 });
